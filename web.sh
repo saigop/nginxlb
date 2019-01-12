3de8eb91805442b647ec6fdb2b714040698d0689
saigop <saigop@gmail.com>
2019-01-12 18:35:11 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -3,5 +3,5 @@
 echo 'Starting Provision: web'$1
 sudo apt-get update
 sudo apt-get install -y nginx
-echo "Machine: web"$1 >> /usr/share/nginx/html/index.html
+echo "Host: Machine"$1 >> /usr/share/nginx/html/index.html
 echo 'Provision web'$1 'complete'
